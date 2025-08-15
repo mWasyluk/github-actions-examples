6bbb28e51fc20313927e2d0c390a8dfd2fe7ae04
mWasyluk <marekwasyluk@wp.pl>
2025-08-16 00:00:20 +0200

    move containers run from compose to run.sh to execute it on a remote server with a single script, without involving FTP

--- 04-cache-dependencies/docker/run.sh
+++ 04-cache-dependencies/docker/run.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+API_CONT_NAME=todosapp-api
+APP_CONT_NAME=todosapp-app
+DB_CONT_NAME=${TODOSAPP_DB_NAME:=todosapp-db}
 
 DOCKER_COMPOSE_PATH="docker/docker-compose.yml"
 
-OPTIONAL_VARS=("TODOSAPP_APP_PORT" "TODOSAPP_DB_PORT" "TODOSAPP_DB_HOST" "TODOSAPP_DB_NAME" "TODOSAPP_DB_USERNAME")
+OPTIONAL_VARS=("TODOSAPP_APP_PORT" "TODOSAPP_DB_HOST" "TODOSAPP_DB_PORT" "TODOSAPP_DB_NAME" "TODOSAPP_DB_USERNAME")
 REQUIRED_VARS=("TODOSAPP_API_IMAGE_NAME" "TODOSAPP_APP_IMAGE_NAME" "TODOSAPP_DB_PASSWORD" "TODOSAPP_ADMIN_SECRET")
 
 # Verify if docker-compose is present in the docker directory
@@ -26,15 +29,15 @@ if [ ${#MISSING_VARS[@]} -gt 0 ]; then
 fi
 
 TODOSAPP_APP_PORT=${TODOSAPP_APP_PORT:=8080}
-TODOSAPP_DB_PORT=${TODOSAPP_DB_PORT:=5432}
-TODOSAPP_DB_USERNAME=${TODOSAPP_DB_NAME:=todosapp-db}
-TODOSAPP_DB_NAME=${TODOSAPP_DB_NAME:=$TODOSAPP_DB_USERNAME}
 TODOSAPP_DB_HOST=${TODOSAPP_DB_HOST:=todosapp-db}
+TODOSAPP_DB_PORT=${TODOSAPP_DB_PORT:=5432}
+TODOSAPP_DB_NAME=${TODOSAPP_DB_NAME:=$TODOSAPP_DB_HOST}
+TODOSAPP_DB_USERNAME=${TODOSAPP_DB_USERNAME:=$TODOSAPP_DB_NAME}
 
 ALL_VARS=("${OPTIONAL_VARS[*]}" "${REQUIRED_VARS[*]}")
 
 # Log values of the variables used by this script
-echo "[LOG] Running containers with the following configuration:"
+echo "[LOG] Environment:"
 for VAR in ${ALL_VARS[@]}; do
     export $VAR=${!VAR}
     case $VAR in
@@ -45,4 +48,64 @@ for VAR in ${ALL_VARS[@]}; do
     printf '      %s\n' "$VAR=$VAL"
 done
 
-docker compose -f $DOCKER_COMPOSE_PATH up
+# Remove current containers if present; pull and run the newest
+echo "[INFO] Clearing space and pulling images..."
+docker rm -f $API_CONT_NAME $APP_CONT_NAME $DB_CONT_NAME >/dev/null 2>&1 || true
+docker network inspect app-net >/dev/null 2>&1 || docker network create app-net
+
+docker pull $TODOSAPP_API_IMAGE_NAME 1>/dev/null 2>&1
+docker pull $TODOSAPP_APP_IMAGE_NAME 1>/dev/null 2>&1
+
+echo "[INFO] Running database..."
+docker run -d \
+  --name "${TODOSAPP_DB_HOST}" \
+  --restart always \
+  --network app-net \
+  -e POSTGRES_DB="$TODOSAPP_DB_NAME" \
+  -e POSTGRES_USER="$TODOSAPP_DB_USERNAME" \
+  -e POSTGRES_PASSWORD="$TODOSAPP_DB_PASSWORD" \
+  --health-cmd "pg_isready -d $TODOSAPP_DB_NAME -U $TODOSAPP_DB_USERNAME" \
+  --health-interval 2s \
+  --health-timeout 10s \
+  --health-retries 10 \
+  --health-start-period 2s \
+  -v app-data:/var/lib/postgresql/data \
+  postgres:17.5-alpine3.21 1>/dev/null
+
+until [ "$(docker inspect -f "{{if .State.Health}}{{.State.Health.Status}}{{end}}" "$TODOSAPP_DB_HOST")" = "healthy" ]; do
+    sleep 1
+done
+
+echo "[INFO] ✅ database is healthy. Running api..."
+
+docker run -d \
+  --name "$API_CONT_NAME" \
+  --restart always \
+  --network app-net \
+  -e TODOSAPP_DB_HOST="$TODOSAPP_DB_HOST" \
+  -e TODOSAPP_DB_PORT="$TODOSAPP_DB_PORT" \
+  -e TODOSAPP_DB_NAME="$TODOSAPP_DB_NAME" \
+  -e TODOSAPP_DB_USERNAME="$TODOSAPP_DB_USERNAME" \
+  -e TODOSAPP_DB_PASSWORD="$TODOSAPP_DB_PASSWORD" \
+  -e TODOSAPP_ADMIN_SECRET="$TODOSAPP_ADMIN_SECRET" \
+  --health-cmd "wget --spider -q http://localhost:8080/health || exit 1" \
+  --health-interval 5s \
+  --health-timeout 10s \
+  --health-retries 10 \
+  --health-start-period 5s \
+  "$TODOSAPP_API_IMAGE_NAME" 1>/dev/null
+
+until [ "$(docker inspect -f "{{if .State.Health}}{{.State.Health.Status}}{{end}}" "$API_CONT_NAME")" = "healthy" ]; do
+    sleep 1
+done
+
+echo "[INFO] ✅ api is healthy. Running app..."
+
+docker run -d \
+  --name "$APP_CONT_NAME" \
+  --restart always \
+  --network app-net \
+  -p "$TODOSAPP_APP_PORT:80" \
+  "$TODOSAPP_APP_IMAGE_NAME" 1>/dev/null
+
+echo "[INFO] ✅ app is healthy. All is up and running."
